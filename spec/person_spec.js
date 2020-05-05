7b36a877d8db145f278fd3e1d70151bb1fe6e192
Siyabulela <siyabulelakhumalo079@gmail.com>
2020-05-05 14:23:11 +0200

    Updated OOP Person

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,16 +1,25 @@
-const { person } = require("../src/person")
+const { Person } = require("../src/person")
 
-describe("finds", function() {
+let person = new Person('Ryan', 30, 'male', ['being a hardarse', ' agile', 'ssd hard drives'])
+describe("person", function() {
     it("should compare name", function() {
-        expect(person.name).toEqual("Ryan")
+
+        expect(person.name).toBe(`Ryan`)
     })
     it("should compare age", function() {
+
         expect(person.age).toEqual(30)
     })
     it("should compare gender", function() {
-        expect(person.gender).toEqual("male")
+
+        expect(person.gender).toBe("male")
     })
     it("should compare interests", function() {
+
         expect(person.interests).toEqual(['being a hardarse', ' agile', 'ssd hard drives'])
+    })
+    it("should compare interests", function() {
+
+        expect(person.hello()).toEqual(`Hello, my name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.`)
     })
 })
--- src/person.js
+++ src/person.js
@@ -7,10 +7,11 @@ class Person {
     }
 
     hello() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interests[0]},${this.interests[1]} and ${this.interests[2]}.`)
+        return (`Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interests[0]},${this.interests[1]} and ${this.interests[2]}.`)
     }
 }
 let person = new Person('Ryan', 30, 'male', ['being a hardarse', ' agile', 'ssd hard drives'])
 let greeting = person.hello()
+console.log(greeting)
 
-module.exports = { person }
+module.exports = { Person }
